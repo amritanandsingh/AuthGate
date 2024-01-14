f3649b9c56477f9cca4825473b8aee643086d8cb
Amrit Anand Singh <50056654+amritanandsingh@users.noreply.github.com>
2024-01-14 11:00:29 +0530

    signin.html is Done

--- authentication/views.py
+++ authentication/views.py
@@ -1,12 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.shortcuts import redirect,render
 # Create your views here.
 
 def home(request):
     return render(request , "authentication/index.html")
 
 def signup(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        fname = request.post.get("fname")
+        lname = request.post.get("lname")
+        email = request.post.get("email")
+        password = request.post.get("password")
+        password2 = request.post.get("password2")
+        
+        myuser = User.objects.create_user(username,email , password)
+        myuser.first_name = fname
+        myuser.last_name = lname
+        myuser.save()
+        messages.success(request , "Your account has been sucessfully created.")
+        return redirect('signin')
+
     return render(request, "authentication/signup.html" )
 
 def signin(request):
