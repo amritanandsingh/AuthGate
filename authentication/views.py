c4fe7f9f447d8edb04e07ed38417f29037092422
Amrit Anand Singh <50056654+amritanandsingh@users.noreply.github.com>
2024-01-14 21:52:43 +0530

    login , logout , signup all function is done

--- authentication/views.py
+++ authentication/views.py
@@ -2,32 +2,49 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
-from django.shortcuts import redirect,render
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login, logout
+
 # Create your views here.
 
 def home(request):
-    return render(request , "authentication/index.html")
+    return render(request, "authentication/index.html")
 
 def signup(request):
     if request.method == "POST":
         username = request.POST.get("username")
-        fname = request.post.get("fname")
-        lname = request.post.get("lname")
-        email = request.post.get("email")
-        password = request.post.get("password")
-        password2 = request.post.get("password2")
+        fname = request.POST.get("fname")  # Corrected typo here
+        lname = request.POST.get("lname")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        password2 = request.POST.get("password2")
         
-        myuser = User.objects.create_user(username,email , password)
+        myuser = User.objects.create_user(username, email, password)
         myuser.first_name = fname
         myuser.last_name = lname
         myuser.save()
-        messages.success(request , "Your account has been sucessfully created.")
+        messages.success(request, "Your account has been successfully created.")
         return redirect('signin')
 
-    return render(request, "authentication/signup.html" )
+    return render(request, "authentication/signup.html")
 
 def signin(request):
-    return render(request, "authentication/signin.html" )
+    if request.method == "POST":  # Check if the request method is POST
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            fname = user.first_name
+            return render(request, "authentication/index.html", {'fname': fname})
+        else:
+            messages.error(request, "Bad credentials")
+            return redirect('home')
+
+    return render(request, "authentication/signin.html")
 
 def signout(request):
-    pass
+    logout(request)
+    messages.success(request , "logged out sucessfully ")
+    return redirect('home')
